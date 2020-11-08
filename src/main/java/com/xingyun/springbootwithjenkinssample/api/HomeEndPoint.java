897b1c8c385c1a6838b817e0419fb83dd2934f3f
qing-feng.zhao <fairy_xingyun@hotmail.com>
2020-11-08 20:40:54 +0800

    update Jenkinsfile

--- src/main/java/com/xingyun/springbootwithjenkinssample/api/HomeEndPoint.java
+++ src/main/java/com/xingyun/springbootwithjenkinssample/api/HomeEndPoint.java
@@ -1,5 +1,6 @@
 package com.xingyun.springbootwithjenkinssample.api;
 
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,8 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class HomeEndPoint {
+    private final Environment environment;
+    public HomeEndPoint(Environment environment) {
+        this.environment = environment;
+    }
     @GetMapping(value = "/")
-    public String home(){
-        return "home";
+    public String Home(){
+        return "hello world,I am "+this.environment.getProperty("spring.application.name");
     }
 }
